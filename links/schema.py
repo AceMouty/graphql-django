a24c21b3e531953e458ddd9c645b76c5045b9ed9
kyle Guerrero <kyleguerrero85@gmail.com>
2020-02-02 22:54:42 -0600

    created a mutation

--- links/schema.py
+++ links/schema.py
@@ -3,6 +3,8 @@ from graphene_django import DjangoObjectType
 
 from .models import Link
 
+# Createing a query
+
 
 class LinkType(DjangoObjectType):
     class Meta:
@@ -14,3 +16,31 @@ class Query(graphene.ObjectType):
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+# ================================================
+
+# Creating a resolver
+
+
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
